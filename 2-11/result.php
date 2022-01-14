5ace008bd381f1e0c8e1ef3cf923f8eb87f12b6d
hitomi2276 <97111229+hitomi2276@users.noreply.github.com>
2022-01-14 16:45:25 +0900

    2-11
    2-11

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$password = $_POST['password'];
+$e_mail = $_POST['e_mail'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>私のメールアドレスは、<?php echo $e_mail; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
